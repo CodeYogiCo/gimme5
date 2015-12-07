566fb524602d30c0ca6f65e21495a2ad69b88386
Vishal Vaibhav <iveeshal@hotmail.com>
2015-12-07 23:03:06 +0530

    just adding controller

--- src/main/java/com/phantom/controller/RestaurantResource.java
+++ src/main/java/com/phantom/controller/RestaurantResource.java
@@ -1,33 +1,51 @@
-package com.phantom.controller;
+    package com.phantom.controller;
 
-import com.phantom.entity.Restaurants;
-import com.phantom.model.RestaurantDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+    import com.phantom.entity.Restaurants;
+    import com.phantom.model.RestaurantDao;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.RequestBody;
+    import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.bind.annotation.RequestMethod;
+    import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by vishal on 21/11/15.
- */
+    import java.util.List;
 
-@RequestMapping(value="/gimme5")
-@RestController
-public class RestaurantResource {
+    /**
+     * Created by vishal on 21/11/15.
+     */
 
-    @Autowired
-    private RestaurantDao restaurantDao;
+    @RequestMapping(value="/gimme5")
+    @RestController
+    public class RestaurantResource {
 
-    @RequestMapping(value="/user/signup" ,method= RequestMethod.POST,headers = "content-type=application/x-www-form-urlencoded;charset=UTF-8" ,
-            produces={"application/json"}
-    )
-    public ResponseEntity addQuestion(@RequestBody Restaurants restaurants){
-        restaurantDao.addRestaurants(restaurants);
-        return ResponseEntity.ok("created");
+        @Autowired
+        private RestaurantDao restaurantDao;
+
+
+        @RequestMapping(value="/restaurant",method=RequestMethod.POST,headers="content-type=application/vnd.v0+json"
+        ,produces = "application/json")
+            public ResponseEntity.BodyBuilder addRestaurants(@RequestBody Restaurants restaurants){
+            try{
+                restaurantDao.addRestaurants(restaurants);
+                return ResponseEntity.ok();
+            }
+            catch(Exception ex){
+                return ResponseEntity.accepted();
+            }
+
+
+            }
+
+        @RequestMapping(value="/restaurant",method=RequestMethod.GET,headers="content-type=application/vnd.v0+json"
+                ,produces = "application/json")
+        public List<Restaurants> getAllRestaurants(@RequestBody Restaurants restaurants){
+            
+                return restaurantDao.getAllRestaurants();
+
+        }
+
+
+
+
     }
-
-
-
-}
